f720c650332ae8fb576536563b765abdb39b0261
shailvestein <62176093+shailvestein@users.noreply.github.com>
2021-12-28 15:07:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,11 +54,11 @@ st.markdown(
 # Header of streamlit webpage
 header_title = '<p style="color:Black; font-size: 40px;">Hand written digit recognition</p>'
 st.markdown(header_title, unsafe_allow_html=True)
-# header_text_1 = '<p style="color:Black; font-size: 20px;">1.  This is AI based web-app, it uses support-vector-machine algorithm in backend to find polarity score.</p>'
 header_text_1 = '<p style="color:Black; font-size: 20px;">1.  This is AI based web-app.</p>'
-header_text_2 = '<p style="color:Black; font-size: 20px;">2.  It tells which numerical digit is in the picture.</p>' # This app is 86% confident that given review is good/bad.</p>'
-text_1 = '<p style="color:Black; font-size: 20px;">The image dimension ratio should be 1:1 for best result.</p>'
-text_2 = '<p style="color:Black; font-size: 20px;">The digit written by hand should be bold and large. Use thick marker to write digit for better result.</p>'
+header_text_2 = '<p style="color:Black; font-size: 20px;">2.  It tells which numerical digit is in the picture.</p>'
+header_text_3 = '<p style="color:Black; font-size: 20px;">3.  The image dimension ratio should be 1:1 for best result.</p>'
+header_text_4 = '<p style="color:Black; font-size: 20px;">4.  The digit written by hand should be bold and large.</p>'
+header_text_5 = '<p style="color:Black; font-size: 20px;">5.  Use thick marker to write digit for better result.</p>'
 st.markdown(header_text_1, unsafe_allow_html=True)
 st.markdown(header_text_2, unsafe_allow_html=True)
 st.markdown(text_1, unsafe_allow_html=True)
